07dfe2d8d4b898cff396f15347384a15d27c821d
Chris Gill <chris@chrisgill.net>
2021-11-19 07:40:01 -0500

    Add some basic configuration in the main file.

--- code.py
+++ code.py
@@ -2,13 +2,18 @@
 # CobraBay
 ####
 
+config = {
+    'max_detect_range': 276, # Range in inches where tracking starts.
+    'approach_strobe_speed': 100 # Strobe speed for the approach lights. In Milliseconds.
+}
+
 # Release any previous displays.
 import displayio
 displayio.release_displays()
 
 # Initialize CobraBay object
 from cobrabay import CobraBay
-cobrabay = CobraBay()
+cobrabay = CobraBay(config)
 print(cobrabay._ApproachStrobe())
 
 # Begin loop
