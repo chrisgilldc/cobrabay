39cab5b20c47a15f743c234cb2e24c2944e5c5cf
Chris <chris@chrisgill.net>
2022-07-04 08:07:37 -0400

    Switched to canvas and swaponsync. Need to fix text flickering and positioning.

--- lib/cobrabay/display.py
+++ lib/cobrabay/display.py
@@ -6,11 +6,7 @@
 
 import logging
 from time import monotonic_ns
-from adafruit_display_shapes.line import Line
-from adafruit_display_text.label import Label
-from adafruit_display_text.bitmap_label import Label
 from math import floor
-import pint
 from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 from .nan import NaN
 from pint import UnitRegistry, Quantity
@@ -72,29 +68,35 @@ class Display:
             'yellow': graphics.Color(255,255,0)
         }
 
+        self._staging_canvas = self.matrix.CreateFrameCanvas()
+
     # Basic frame for the display
-    def _frame(self):
+    def _frame(self,canvas):
         # Left approach box
-        self._draw_box(0, 0, 2, self.matrix.height-5, self._colors['white'])
+        self._draw_box(canvas, 0, 0, 2, canvas.height-5, self._colors['white'])
         # Right approach box
-        self._draw_box(self.matrix.width-3, 0, self.matrix.width-1, self.matrix.height-5, self._colors['white'])
-        # Approach strobe
-        self._draw_box(0, self.matrix.height-3, self.matrix.width-1, self.matrix.height-1, self._colors['white'])
+        self._draw_box(canvas, canvas.width-3, 0, canvas.width-1, canvas.height-5, self._colors['white'])
+        # Approach strobe box
+        self._draw_box(canvas, 0, canvas.height-3, canvas.width-1, canvas.height-1, self._colors['white'])
+        # Return the canvas for additional additions.
+        return canvas
 
     # Utility method to make drawing boxes easier.
-    def _draw_box(self, x1, y1, x2, y2, color=None):
+    def _draw_box(self, canvas, x1, y1, x2, y2, color=None):
         if color is None:
             color = graphics.Color(255, 255, 255)
         # Top line
-        graphics.DrawLine(self.matrix, x1, y1, x2, y1, color)
+        graphics.DrawLine(canvas, x1, y1, x2, y1, color)
         # Left side
-        graphics.DrawLine(self.matrix, x1, y1, x1, y2, color)
+        graphics.DrawLine(canvas, x1, y1, x1, y2, color)
         # Right side
-        graphics.DrawLine(self.matrix, x2, y1, x2, y2, color)
+        graphics.DrawLine(canvas, x2, y1, x2, y2, color)
         # Bottom
-        graphics.DrawLine(self.matrix, x1, y2, x2, y2, color)
+        graphics.DrawLine(canvas, x1, y2, x2, y2, color)
+        # Send the canvas back.
+        return canvas
 
-    def _distance_display(self, range, range_pct):
+    def _distance_display(self, canvas, range, range_pct):
         start_x = 4
         # Lower box is three pixels, allow one for buffer, and adjust one more for 0-1 adjustment.
         start_y = self.matrix.height - 4
@@ -136,7 +138,9 @@ class Display:
             else:
                 text_color = self._colors['white']
 
-        graphics.DrawText(self.matrix,self.base_font,start_x,start_y,text_color,text)
+        graphics.DrawText(canvas,self.base_font,start_x,start_y,text_color,text)
+
+        return canvas
 
     # @ureg.check('[length]',(None))
     @staticmethod
@@ -147,7 +151,7 @@ class Display:
         inches = round(length.to(ureg.inch).magnitude % 12,int_places)
         return "{}'{}\"".format(feet,inches)
 
-    def _approach_strobe(self, range, range_pct):
+    def _approach_strobe(self, canvas, range, range_pct):
         # Available strobing width. Cut in half since we go from both sides, take out pixels for the frames.
         available_width = (self.matrix.width / 2) - 1
 
@@ -158,27 +162,18 @@ class Display:
             # Only draw a blocker bar if it would have length.
             if bar_blocker > 0:
                 # Left
-                graphics.DrawLine(self.matrix, 1,
-                                  self.matrix.height - 2,
+                graphics.DrawLine(canvas, 1,
+                                  canvas.height - 2,
                                   1 + bar_blocker,
-                                  self.matrix.height - 2,
+                                  canvas.height - 2,
                                   self._colors['white'])
                 # Right
-                graphics.DrawLine(self.matrix,
-                                  self.matrix.width - 2,
-                                  self.matrix.height - 2,
-                                  self.matrix.width - 2 - bar_blocker,
-                                  self.matrix.height - 2,
+                graphics.DrawLine(canvas,
+                                  canvas.width - 2,
+                                  canvas.height - 2,
+                                  canvas.width - 2 - bar_blocker,
+                                  canvas.height - 2,
                                   self._colors['white'])
-
-            # Blank out the strobing space.
-            graphics.DrawLine(self.matrix,
-                              2 + bar_blocker,
-                              self.matrix.height - 2,
-                              self.matrix.width - 2 - bar_blocker,
-                              self.matrix.height - 2,
-                              self._colors['black']
-                              )
 
             # Determine strober offset based on time.
             if monotonic_ns() - self.timer_approach_strobe >= self.config['approach_strobe_speed']:
@@ -189,134 +184,147 @@ class Display:
                 self.timer_approach_strobe = monotonic_ns()
 
             # Draw dots based on the offset.
-            graphics.DrawLine(self.matrix,
+            graphics.DrawLine(canvas,
                               1 + bar_blocker + self._approach_strobe_offset,
-                              self.matrix.height - 2,
+                              canvas.height - 2,
                               1 + bar_blocker + self._approach_strobe_offset + 1,
-                              self.matrix.height - 2,
+                              canvas.height - 2,
                               self._colors['red']
                               )
-            graphics.DrawLine(self.matrix,
-                              self.matrix.width - bar_blocker - self._approach_strobe_offset,
-                              self.matrix.height - 2,
-                              self.matrix.width - bar_blocker - self._approach_strobe_offset-1,
-                              self.matrix.height - 2,
+            graphics.DrawLine(canvas,
+                              canvas.width - bar_blocker - self._approach_strobe_offset,
+                              canvas.height - 2,
+                              canvas.width - bar_blocker - self._approach_strobe_offset-1,
+                              canvas.height - 2,
                               self._colors['red']
                               )
+        # Pass the Canvas back for further updates.
+        return canvas
 
-    def _side_indicators(self, lateral, lateral_num):
-        si_group = Group()
-        
-        # Assign out the available vertical pixels in a round-robin fashion so each area gets a fair shake.
-        area_lengths = [0] * lateral_num
-        available_height = self.display.height-2
-        candidate_area = 0
-        while available_height > 0:
-            area_lengths[candidate_area] += 1
-            available_height -= 1
-            candidate_area += 1
-            if candidate_area > len(area_lengths) - 1:
-                candidate_area = 0
-        
-        # Start position of the bar, from the top.
-        bar_start = 1
-        # Go through each lateral detection zone. These should be ordered from rear to front.
-        for index in range(len(lateral)):
-            # If it returned the string 'BR', the vehicle has yet to cross this sensor's line of sight, so do nothing.
-            if isinstance(lateral[index], str):
-                if lateral[index] == 'BR':
-                    pass  # Append to both left and right.
-            # Show Green if we're in the 'spot on' or 'acceptable' deviance statuses.
-            elif lateral[index]['status'] <= 1:
-                line_left = Line(1, bar_start, 1, bar_start+area_lengths[index]-1, 0x00FF00)
-                line_right = Line(self.display.width-2, bar_start, self.display.width-2,
-                                  bar_start+area_lengths[index]-1, 0x00FF00)
-                # if things are spot on, light both sides.
-                if lateral[index]['direction'] is None:
-                    si_group.append(line_left)
-                    si_group.append(line_right)
-                # If left, light left.
-                elif lateral[index]['direction'] == 'L':
-                    si_group.append(line_left)
-                # If right, light right.
-                elif lateral[index]['direction'] == 'R':
-                    si_group.append(line_right)
-            elif lateral[index]['status'] == 2:
-                line_left = Line(1, bar_start, 1, bar_start+area_lengths[index]-1, 0xFFFF00)
-                line_right = Line(self.display.width-2, bar_start, self.display.width-2,
-                                  bar_start+area_lengths[index]-1, 0xFFFF00)
-                if lateral[index]['direction'] == 'L':
-                    si_group.append(line_left)
-                elif lateral[index]['direction'] == 'R':
-                    si_group.append(line_right)
-            elif lateral[index]['status'] == 3:
-                line_left = Line(1, bar_start, 1, bar_start+area_lengths[index]-1, 0xFF0000)
-                line_right = Line(self.display.width-2, bar_start, self.display.width-2,
-                                  bar_start+area_lengths[index]-1, 0xFF0000)
-                # if things are spot on, light both sides.
-                if lateral[index]['direction'] == 'L':
-                    si_group.append(line_left)
-                elif lateral[index]['direction'] == 'R':
-                    si_group.append(line_right)
-
-            # Increment so the next bar starts in the correct place.
-            bar_start = bar_start + area_lengths[index]
-        return si_group   
+    # def _side_indicators(self, lateral, lateral_num):
+    #     si_group = Group()
+    #
+    #     # Assign out the available vertical pixels in a round-robin fashion so each area gets a fair shake.
+    #     area_lengths = [0] * lateral_num
+    #     available_height = self.display.height-2
+    #     candidate_area = 0
+    #     while available_height > 0:
+    #         area_lengths[candidate_area] += 1
+    #         available_height -= 1
+    #         candidate_area += 1
+    #         if candidate_area > len(area_lengths) - 1:
+    #             candidate_area = 0
+    #
+    #     # Start position of the bar, from the top.
+    #     bar_start = 1
+    #     # Go through each lateral detection zone. These should be ordered from rear to front.
+    #     for index in range(len(lateral)):
+    #         # If it returned the string 'BR', the vehicle has yet to cross this sensor's line of sight, so do nothing.
+    #         if isinstance(lateral[index], str):
+    #             if lateral[index] == 'BR':
+    #                 pass  # Append to both left and right.
+    #         # Show Green if we're in the 'spot on' or 'acceptable' deviance statuses.
+    #         elif lateral[index]['status'] <= 1:
+    #             line_left = Line(1, bar_start, 1, bar_start+area_lengths[index]-1, 0x00FF00)
+    #             line_right = Line(self.display.width-2, bar_start, self.display.width-2,
+    #                               bar_start+area_lengths[index]-1, 0x00FF00)
+    #             # if things are spot on, light both sides.
+    #             if lateral[index]['direction'] is None:
+    #                 si_group.append(line_left)
+    #                 si_group.append(line_right)
+    #             # If left, light left.
+    #             elif lateral[index]['direction'] == 'L':
+    #                 si_group.append(line_left)
+    #             # If right, light right.
+    #             elif lateral[index]['direction'] == 'R':
+    #                 si_group.append(line_right)
+    #         elif lateral[index]['status'] == 2:
+    #             line_left = Line(1, bar_start, 1, bar_start+area_lengths[index]-1, 0xFFFF00)
+    #             line_right = Line(self.display.width-2, bar_start, self.display.width-2,
+    #                               bar_start+area_lengths[index]-1, 0xFFFF00)
+    #             if lateral[index]['direction'] == 'L':
+    #                 si_group.append(line_left)
+    #             elif lateral[index]['direction'] == 'R':
+    #                 si_group.append(line_right)
+    #         elif lateral[index]['status'] == 3:
+    #             line_left = Line(1, bar_start, 1, bar_start+area_lengths[index]-1, 0xFF0000)
+    #             line_right = Line(self.display.width-2, bar_start, self.display.width-2,
+    #                               bar_start+area_lengths[index]-1, 0xFF0000)
+    #             # if things are spot on, light both sides.
+    #             if lateral[index]['direction'] == 'L':
+    #                 si_group.append(line_left)
+    #             elif lateral[index]['direction'] == 'R':
+    #                 si_group.append(line_right)
+    #
+    #         # Increment so the next bar starts in the correct place.
+    #         bar_start = bar_start + area_lengths[index]
+    #     return si_group
 
     # Draw signal bars with an origin of X,Y
-    def _signal_bars(self, strength, origin):
+    def _signal_bars(self, canvas, strength, origin):
         # If signal strength is 0, then show an X since we're probably offline.
         if strength < 1:
-            graphics.DrawLine(self.matrix, origin[0], origin[1], origin[0] + 4, origin[1] + 4, self._colors['red'])
-            graphics.DrawLine(self.matrix, origin[0], origin[1] + 4, origin[0] + 4, origin[1], self._colors['red'])
+            graphics.DrawLine(canvas, origin[0], origin[1], origin[0] + 4, origin[1] + 4, self._colors['red'])
+            graphics.DrawLine(canvas, origin[0], origin[1] + 4, origin[0] + 4, origin[1], self._colors['red'])
         else:
             i = 1
             while i <= strength:
                 graphics.DrawLine(
-                    self.matrix,
+                    canvas,
                     origin[0]-1+i,
                     origin[1]+5,
                     origin[0]-1+i,
                     origin[1]+(5-i),
                     self._colors['green'])
                 i += 1
+        return canvas
 
     # Show the MQTT connection status icon.
-    def _mqtt_icon(self, mqtt_status, origin):
+    def _mqtt_icon(self, canvas, mqtt_status, origin):
         if mqtt_status:
             color_fg = self._colors['darkblue']
         else:
             color_fg = self._colors['red']
         # Left side
-        graphics.DrawLine(self.matrix, origin[0], origin[1], origin[0], origin[1]+4, color_fg)
+        graphics.DrawLine(canvas, origin[0], origin[1], origin[0], origin[1]+4, color_fg)
         # Right side
-        graphics.DrawLine(self.matrix, origin[0]+4, origin[1], origin[0]+4, origin[1]+4, color_fg)
+        graphics.DrawLine(canvas, origin[0]+4, origin[1], origin[0]+4, origin[1]+4, color_fg)
         # Left slant.
-        graphics.DrawLine(self.matrix, origin[0], origin[1], origin[0]+2, origin[1]+2, color_fg)
+        graphics.DrawLine(canvas, origin[0], origin[1], origin[0]+2, origin[1]+2, color_fg)
         # Right slant
-        graphics.DrawLine(self.matrix, origin[0]+2, origin[1]+2, origin[0]+4, origin[1], color_fg)
+        graphics.DrawLine(canvas, origin[0]+2, origin[1]+2, origin[0]+4, origin[1], color_fg)
+
+        # Send the canvas back
+        return canvas
 
     # Display state icons, and optionally a single sensor.
     def display_generic(self, system_state, sensor=None):
+        canvas = self.matrix.CreateFrameCanvas()
         if sensor is not None:
-            graphics.DrawText(self.matrix,
+            graphics.DrawText(canvas,
                               self.base_font,
                               5,
-                              self.matrix.height - 10,
+                              canvas.height - 10,
                               self._colors['white'],
                               sensor
                               )
-        self._signal_bars(system_state['signal_strength'], (59, 27))
-        self._mqtt_icon(system_state['mqtt_status'], (0, 27))
+        self._signal_bars(canvas,system_state['signal_strength'], (59, 27))
+        self._mqtt_icon(canvas,system_state['mqtt_status'], (0, 27))
+        self.matrix.SwapOnVSync(canvas)
 
     # Update the display from the provided bay state.
     def display_dock(self, bay_state):
+        # Create a new canvas to display
+        self._staging_canvas.Clear()
         # Frame is always constant, so we add this right away.
         # Display the Frame
-        self._frame()
+        #self._staging_canvas = self._frame(self._staging_canvas)
         # Display the strobe based on range. 
-        self._approach_strobe(bay_state['range'], bay_state['range_pct'])
+        #self._staging_canvas = self._approach_strobe(self._staging_canvas, bay_state['range'], bay_state['range_pct'])
         # Display the distance.
-        self._distance_display(bay_state['range'], bay_state['range_pct'])
+        self._staging_canvas = self._distance_display(self._staging_canvas, bay_state['range'], bay_state['range_pct'])
+        #self._distance_display(self.matrix, bay_state['range'], bay_state['range_pct'])
         # Display the side indicators
         #master_group.append(self._side_indicators(bay_state['lateral'], bay_state['lateral_num']))
+        # Swap in the new canvas.
+        self.matrix.SwapOnVSync(self._staging_canvas)
