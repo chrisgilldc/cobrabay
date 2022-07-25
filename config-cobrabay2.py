e550c924bd9a97f64effaefb0097a3dd46d37dd9
Chris <chris@chrisgill.net>
2022-07-24 21:07:52 -0400

    Test configs.

--- config-cobrabay2.py
+++ config-cobrabay2.py
@@ -5,28 +5,30 @@ config = {
         'sensor_pacing': 5, # Time in seconds between each ultrasonic sensor ping, to prevent echos.
         'system_name': 'CobraBay2', # ID of the system. Will be used for MQTT client ID, as well as name in MQTT Topics.
         'homeassistant': False,  # Integrate with Home Assistant?
-        'syslog':
-             {'host': 'tachyon.jumpbeacon.net',
-              'facility': 'local7',
-              'protocol': 'udp'}
+        # 'syslog':
+        #      {'host': 'tachyon.jumpbeacon.net',
+        #       'facility': 'local7',
+        #       'protocol': 'udp'}
         },
     # Define sensors to be used in the Bay definition.
     'sensors': {
         #'center': {'type': 'vl53', 'address': 0x29, 'distance_mode': 'long', 'timing_budget': 50 },
         #'lat_front': {'type': 'hcsr04', 'board': 0x58, 'trigger': 1, 'echo': 2, 'timeout': 0.5, 'avg': 5 },
         #'lat_rear': {'type': 'hcsr04', 'board': 0x58, 'trigger': 3, 'echo': 4, 'timeout': 0.5, 'avg': 5 },
-        'center': {'type': 'synth', 'role': 'approach', 'start_value': 762, 'delta-d': 1 },
+        'center': {'type': 'synth', 'role': 'approach', 'start_value': 50, 'delta-d': 1 },
         'lat_front': {'type': 'synth', 'role': 'side', 'start_value': 10, 'variance': 10 },
         'lat_rear': {'type': 'synth', 'role': 'side', 'start_value': 10, 'variance': 10 }
         },
     'bay': {  # Bay definition. ONLY ONE IS SUPPORTED NOW!
         'active': True,  # Is the bay active?
         'name': 'bay2',  # Name to use for the bay in MQTT. Will be made all lower-case.
-        'park_time': 180,  # How long until a stationary vehicle is counted as parked. In Seconds.
+        'park_time': "2 min",  # How long until a stationary vehicle is counted as parked.
         # How to range-find the vehicle
         'range': {
-            'dist_max': "276 in", # Maximum range to report at
-            'dist_stop': "10 in", # Distance from the range sensor where the vehicle should stop.
+            # 'dist_max': "276 in", # Maximum range to report at
+            'dist_max': "40 in", # Maximum range to report at
+            # 'dist_stop': "10 in", # Distance from the range sensor where the vehicle should stop.
+            'dist_stop': "5 in", # Distance from the range sensor where the vehicle should stop.
             'sensor': 'center' # Assigned sensor for range finding
             },
         # Lateral alignment zones to check if the vehicle is too far left or right.
