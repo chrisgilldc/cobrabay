43f525ec6fbafbc930985ca868bdf00c6ea44fda
Chris <chris@chrisgill.net>
2022-10-10 09:09:50 -0400

    Remove const.py, no longer needed.

--- lib/CONST.py
+++
@@ -1,9 +0,0 @@
-color_names = {
-    'black': graphics.Color(0, 0, 0),
-    'white': graphics.Color(255, 255, 255),
-    'green': graphics.Color(0, 255, 0),
-    'red': graphics.Color(255, 0, 0),
-    'darkblue': graphics.Color(0, 0, 139),
-    'orange': graphics.Color(255, 153, 51),
-    'yellow': graphics.Color(255, 255, 0)
-}
