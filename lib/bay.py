f01563504e83ba67e1e77b36d8addc348c1c5125
Chris <chris@chrisgill.net>
2022-10-25 22:53:47 -0400

    Updated flow for intercept detection logic. Now pass range readings to lateral detectors, that happens there. Add some logic to pick among multiple longitudinal sensors - right now just one, but will be expanded in the future. Additional variable normalization. Fix bug with properly calling start_ranging on VL53L1Xs when docking.

--- lib/bay.py
+++ lib/bay.py
@@ -6,14 +6,16 @@ import pint.errors
 from .detector import Detector, Range, Lateral
 from pint import UnitRegistry, Quantity
 from PIL import Image, ImageDraw
+from .detector import CB_VL53L1X
 import logging
 
 class Bay:
     def __init__(self,config,detectors):
         # Set the Bay ID. This is static for testing, real config processing will come later.
+        self._detectors = None
         self.bay_id = 'bay1'
         try:
-            self.bay_name = config['bay_name']
+            self.bay_name = config['name']
         except KeyError:
             self.bay_name = self.bay_id
         # Create a logger.
@@ -77,7 +79,6 @@ class Bay:
             'bay_name': self.bay_name,
             'detectors': []
         }
-        self._logger.debug("Discovery trying to handle detectors: {}".format(self._detectors))
         for direction in self._detectors:
             for item in self._detectors[direction]:
                 detector = {
@@ -86,7 +87,6 @@ class Bay:
                 }
                 return_dict['detectors'].append(detector)
 
-        self._logger.debug("Bay discovery info: {}".format(return_dict))
         return return_dict
 
     # Bay properties
@@ -122,37 +122,46 @@ class Bay:
 
     # Scans
     def _scan_detectors(self):
-        self._position = {}
-        self._quality = {}
+        self._position = { 'longitudinal': {}, 'lateral': {} }
+        self._quality = { 'longitudinal': {}, 'lateral': {} }
+        self._logger.debug("Starting detector scan.")
+        self._logger.debug("Have detectors: {}".format(self._detectors))
         # Longitudinal offset.
-        self._position['lo'] = self._detectors['range'].value.to(self._output_unit)
-        self._quality['lo'] = self._detectors['range'].quality
-        self._logger.debug("Scan Detector set longitudinal position to {} and quality to {}".
-                           format(self._position['lo'],self._quality['lo']))
-        self._logger.debug("Checking {} lateral detectors".format(len(self._detectors['lateral'])))
-        if len(self._detectors['lateral']) > 0:
-            i = 0
-            self._position['la'] = {}
-            self._quality['la'] = {}
-            for detector_name in self._detectors['lateral']:
-                self._logger.debug("Checking {}".format(detector_name))
-                # Has the vehicle reached the intercept range for this lateral sensor?
-                # If not, we return "NI", Not Intercepted.
-                if self._position['lo'] <= self._detectors['lateral'][detector_name]['intercept']:
-                    detector_value = self._detectors['lateral'][detector_name]['obj'].value
-                    if isinstance(detector_value,str):
-                        self._position['la'][detector_name] = 'BR'
-                        self._quality['la'][detector_name] = 'BR'
-                    else:
-                        self._position['la'][detector_name] = detector_value.to(self._output_unit)
-                        self._quality['la'][detector_name] = self._detectors['lateral'][detector_name]['obj'].quality
-                else:
-                    self._position['la'][detector_name] = 'NI'
-                    self._quality['la'][detector_name] = 'NI'
-                i += 1
-        else:
-            self._position['la'] = None
-            self._quality['la'] = None
+        self._logger.debug("Checking longitudinal detectors.")
+        for detector in self._detectors['longitudinal']:
+            value = self._detectors['longitudinal'][detector].value
+            self._logger.debug("Read of longitudinal detector {} returned {}".format(detector, value))
+            self._position['longitudinal'][detector] = self._detectors['longitudinal'][detector].value.to(self._output_unit)
+            self._quality['longitudinal'][detector] = self._detectors['longitudinal'][detector].quality
+        # Pick a reading for authoritative range.
+        # If there's only one longitudinal sensor, we go with that.
+        if len(self._detectors['longitudinal']) == 1:
+            self._selected_range = list(self._detectors['longitudinal'].keys())[0]
+        self._logger.debug("Selected range detector: {}".format(self._selected_range))
+        for detector_name in self._detectors['lateral']:
+            self._logger.debug("Checking {}".format(detector_name))
+            # Give the lateral detector the current range reading.
+            self._detectors['lateral'][detector_name].range_reading = self._position['longitudinal'][self._selected_range]
+            # Now we can get the position and quality from the detector.
+            self._position['lateral'][detector_name] = self._detectors['lateral'][detector_name].value.to(self._output_unit)
+            self._quality['lateral'][detector_name] = self._detectors['lateral'][detector_name].quality
+        #     # Has the vehicle reached the intercept range for this lateral sensor?
+        #     # If not, we return "NI", Not Intercepted.
+        #     if self._position['lo'] <= self._detectors['lateral'][detector_name]['intercept']:
+        #         detector_value = self._detectors['lateral'][detector_name]['obj'].value
+        #             if isinstance(detector_value,str):
+        #             self._position['la'][detector_name] = 'BR'
+        #                 self._quality['la'][detector_name] = 'BR'
+        #             else:
+        #                 self._position['la'][detector_name] = detector_value.to(self._output_unit)
+        #                 self._quality['la'][detector_name] = self._detectors['lateral'][detector_name]['obj'].quality
+        #         else:
+        #             self._position['la'][detector_name] = 'NI'
+        #             self._quality['la'][detector_name] = 'NI'
+        #         i += 1
+        # else:
+        #     self._position['la'] = None
+        #     self._quality['la'] = None
 
     @property
     def state(self):
@@ -178,13 +187,9 @@ class Bay:
             self._logger.debug("Entering state {}".format(input))
             self._logger.debug("Detectors: {}".format(self._detectors))
             # When entering docking or undocking state, start ranging on the sensors.
-            for detector in self._detectors:
-                self._logger.debug("Activating detector {}".format(detector))
-                self._detectors[detector].activate()
+            self._detector_state('activate')
         if input not in ('docking','undocking') and self._state in ('docking', 'undocking'):
-            # When leaving docking or undocking state, stop ranging on the sensors.
-            for detector in self._detectors:
-                self._detectors[detector].deactivate()
+            self._detector_state('deactivate')
 
     # MQTT status methods. These generate payloads the core network handler can send upward.
     def mqtt_messages(self,verify=False):
@@ -236,6 +241,22 @@ class Bay:
         self._shutdown_detectors(self._detectors)
         self._logger.error("Shutdown complete. Exiting.")
 
+    # Traverse the detectors dict, activate everything that needs activating.
+    def _detector_state(self,mode):
+        if mode not in ('activate','deactivate'):
+            raise ValueError("Bay can only set detector states to 'activate', or 'deactivate'")
+        # Traverse the dict looking for detectors that need activation.
+        for direction in ('longitudinal','lateral'):
+            for detector in self._detectors[direction]:
+                if isinstance(self._detectors[direction][detector],CB_VL53L1X):
+                    if mode == 'activate':
+                        self._detectors[direction][detector].activate()
+                    elif mode == 'deactivate':
+                        self._detectors[direction][detector].deactivate()
+                    else:
+                        raise RuntimeError("Detector activation reached impossible state.")
+
+
     # Traverse the detectors and make sure they're all stopped.
     def _shutdown_detectors(self,detectors):
         if isinstance(detectors,list):
@@ -275,11 +296,12 @@ class Bay:
 
         config_options = {
             'longitudinal': ('offset','spread_park','bay_depth'),
-            'lateral': ('offset','spread_ok','spread_warn','spread_crit','side')
+            'lateral': ('offset','spread_ok','spread_warn','spread_crit','side','intercept')
         }
         for direction in self._detectors.keys():
             self._logger.debug("Setting up {} detectors.".format(direction))
             for detector_config in config[direction]['detectors']:
+                self._logger.debug("Provided detector config: {}".format(detector_config))
                 try:
                     detector_obj = detectors[detector_config['detector']]
                 except KeyError:
@@ -291,43 +313,42 @@ class Bay:
                         setattr(detector_obj, item, detector_config[item])
                     except KeyError:
                         self._logger.debug("Using default value for {}".format(item))
-                    try:
-                        setattr(detector_obj, item, config[direction]['defaults'][item])
-                    except KeyError:
-                        raise KeyError("Needed default value for {} but not defined!".format(item))
-                # If we're processing lateral, add the intercept range to the lateral order dict.
-                if direction == 'lateral':
-                    self._lateral_order[Quantity(detector_config['intercept'])] = detector_config['detector']
+                        try:
+                            setattr(detector_obj, item, config[direction]['defaults'][item])
+                        except KeyError:
+                            raise KeyError("Needed default value for {} but not defined!".format(item))
+                # # If we're processing lateral, add the intercept range to the lateral order dict.
+                # if direction == 'lateral':
+                #     self._lateral_order[Quantity(detector_config['intercept'])] = detector_config['detector']
                 # Append the object to the appropriate object store.
                 self._detectors[direction][detector_config['detector']] = detector_obj
-        # self._lateral_order =
 
-    # Method to set up the range detector
-    def _setup_range(self,config,detectors):
-        # Is the detector name specified.
-        try:
-            range_detector_name = config['range']['detector']
-        except KeyError:
-            raise KeyError("Detector is not defined for bay's range. This is required!")
-        # Try to get the detector.
-        try:
-            self._detectors['range'] = detectors[range_detector_name]
-        except KeyError:
-            raise KeyError("Provided detector name '{}' does not exist.".format(range_detector_name))
-        # Add the required range parameters.
-        self._logger.info("Setting range detector offset to: {}".format(config['range']['offset']))
-        self._detectors['range'].offset = config['range']['offset']
-        self._logger.info("Setting range detector bay depth to: {}".format(config['range']['bay_depth']))
-        self._detectors['range'].bay_depth = config['range']['bay_depth']
-        self._logger.info("Setting parking spread to: {}".format(config['range']['spread_park']))
-        self._detectors['range'].spread_park = config['range']['spread_park']
-        try:
-            self._logger.info("Setting range detector warn to: {}".format(config['range']['pct_warn']))
-            self._detectors['range'].bay_depth = config['range']['pct_warn']
-        except KeyError:
-            pass
-        try:
-            self._logger.info("Setting range detector warn to: {}".format(config['range']['pct_crit']))
-            self._detectors['range'].bay_depth = config['range']['pct_crit']
-        except KeyError:
-            pass
+    # # Method to set up the range detector
+    # def _setup_range(self,config,detectors):
+    #     # Is the detector name specified.
+    #     try:
+    #         range_detector_name = config['range']['detector']
+    #     except KeyError:
+    #         raise KeyError("Detector is not defined for bay's range. This is required!")
+    #     # Try to get the detector.
+    #     try:
+    #         self._detectors['range'] = detectors[range_detector_name]
+    #     except KeyError:
+    #         raise KeyError("Provided detector name '{}' does not exist.".format(range_detector_name))
+    #     # Add the required range parameters.
+    #     self._logger.info("Setting range detector offset to: {}".format(config['range']['offset']))
+    #     self._detectors['range'].offset = config['range']['offset']
+    #     self._logger.info("Setting range detector bay depth to: {}".format(config['range']['bay_depth']))
+    #     self._detectors['range'].bay_depth = config['range']['bay_depth']
+    #     self._logger.info("Setting parking spread to: {}".format(config['range']['spread_park']))
+    #     self._detectors['range'].spread_park = config['range']['spread_park']
+    #     try:
+    #         self._logger.info("Setting range detector warn to: {}".format(config['range']['pct_warn']))
+    #         self._detectors['range'].bay_depth = config['range']['pct_warn']
+    #     except KeyError:
+    #         pass
+    #     try:
+    #         self._logger.info("Setting range detector warn to: {}".format(config['range']['pct_crit']))
+    #         self._detectors['range'].bay_depth = config['range']['pct_crit']
+    #     except KeyError:
+    #         pass
