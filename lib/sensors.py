fe2ff1cef3fafa99fd2fe3d750a7dce3c744cb0c
Chris <chris@chrisgill.net>
2022-10-28 05:34:32 -0400

    Initial TFmini implementation seems to work. Further development will have to wait on a hardware issue.

--- lib/sensors.py
+++ lib/sensors.py
@@ -13,28 +13,35 @@ from adafruit_vl53l1x import VL53L1X as af_VL53L1X
 from pint import Quantity
 from pint import UnitRegistry
 from .TFmini_I2C import TFminiI2C
+from smbus2 import SMBus, i2c_msg
+from .util import scan_i2c
+import sys
 
 
 class BaseSensor:
-    def __init__(self,board_options):
+    def __init__(self, board_options):
         # Check for the Base I2C Sensors
-        required = ('i2c_bus','i2c_address')
+        required = ('i2c_bus', 'i2c_address')
         for item in required:
             if item not in board_options:
                 raise ValueError("Required board_option '{}' missing.".format(item))
         # Create a logger
-        self._name = "{}-{}".format(type(self).__name__,hex(board_options['i2c_address']))
+        self._name = "{}-{}-{}".format(type(self).__name__, board_options['i2c_bus'], hex(board_options['i2c_address']))
         self._logger = logging.getLogger("CobraBay").getChild("Sensors").getChild(self._name)
         self._logger.info("Initializing sensor...")
 
         # Set the I2C bus and I2C Address
         self._logger.debug("Setting I2C Properties...")
-        self._i2c_bus = board_options['i2c_bus']
-        self._i2c_address = board_options['i2c_address']
+        self.i2c_bus = board_options['i2c_bus']
+        self.i2c_address = board_options['i2c_address']
+        self._logger.debug("Now have I2C Bus {} and Address {}".format(self.i2c_bus, hex(self.i2c_address)))
 
         # Create a unit registry for the object.
         self._ureg = UnitRegistry()
+
+        # Initialize variables.
         self._previous_timestamp = monotonic()
+        self._previous_reading = None
 
         # Sensor should call this init and then extend with its own options.
         # super().__init__(board_options)
@@ -45,15 +52,29 @@ class BaseSensor:
         return self._i2c_bus
 
     @i2c_bus.setter
-    def i2c_bus(self,input):
-        if input not in (1,2):
+    def i2c_bus(self, input):
+        if input not in (1, 2):
             raise ValueError("I2C Bus ID for Raspberry Pi must be 1 or 2, not {}".format(input))
         else:
             self._i2c_bus = input
 
     @property
+    def i2c_address(self):
+        return self._i2c_address
+
+    @i2c_address.setter
+    # Stores the address of the board. Does *not* necesarially apply it to the board to update it.
+    def i2c_address(self, i2c_address):
+        # If it's in "0xYY" format, convert it to a base 16 int.
+        if isinstance(i2c_address, str):
+            self._i2c_address = int(i2c_address, base=16)
+        else:
+            self._i2c_address = i2c_address
+
+    @property
     def range(self):
         raise NotImplementedError("Range should be overridden by specific sensor class.")
+
 
 class CB_VL53L1X(BaseSensor):
     _i2c_address: int
@@ -64,37 +85,32 @@ class CB_VL53L1X(BaseSensor):
     def __init__(self, board_options):
         # Call super.
         super().__init__(board_options)
-        # Add self to instance list.
+        # Check for additional required options.
+        required = ['i2c_bus', 'i2c_address', 'enable_board', 'enable_pin']
+        # Store the options.
+        for item in required:
+            if item not in board_options:
+                raise ValueError("Required board_option '{}' missing".format(item))
+
+        # Define the enable pin.
         self._enable_pin = None
+
+        # Add self to instance list.
+
         CB_VL53L1X.instances.add(self)
         self._performance = {
             'max_range': Quantity('4000mm'),
             'min_range': Quantity('30mm')
         }
 
-    def _setup_sensor(self, board_options):
-        # Set a default log level if not defined.
-        try:
-            self._log_level = logging.getLevelName(board_options['log_level'])
-        except KeyError:
-            # Default to warning.
-            self._log_level = logging.WARNING
-
         # Create a board object we can reference.
         self._board = board
-
         # Create access to the I2C bus
         try:
             self._i2c = busio.I2C(board.SCL, board.SDA)
-        except:
+        except ValueError:
             raise
 
-        # Check for required options in
-        options = ['i2c_bus', 'i2c_address', 'enable_board', 'enable_pin']
-        # Store the options.
-        for item in options:
-            if item not in board_options:
-                raise ValueError("Required board_option '{}' missing".format(item))
         # Set the properties
         self.enable_board = board_options['enable_board']
         self.enable_pin = board_options['enable_pin']
@@ -109,7 +125,7 @@ class CB_VL53L1X(BaseSensor):
         self.measurement_time = Quantity(board_options['timing']).to('microseconds').magnitude
         self.distance_mode = 'long'
         self._previous_reading = self._sensor_obj.distance
-
+        self._logger.debug("Test reading: {}".format(self._previous_reading))
         self._sensor_obj.stop_ranging()
 
     def start_ranging(self):
@@ -164,7 +180,6 @@ class CB_VL53L1X(BaseSensor):
     def range(self):
         # Make sure to pace the readings properly, so we're not over-running the native readings.
         # If a request comes in before the sleep time (200ms), return the previous reading.
-
         if monotonic() - self._previous_timestamp < 0.2:
             return self._previous_reading
         else:
@@ -172,7 +187,10 @@ class CB_VL53L1X(BaseSensor):
             if reading is None:
                 return None
             else:
-                return Quantity(reading, self._ureg.centimeter)
+                reading = Quantity(reading, self._ureg.centimeter)
+                self._previous_reading = reading
+                self._previous_timestamp = monotonic()
+                return self._previous_reading
 
     # Method to find out if an address is on the I2C bus.
     def _addr_on_bus(self, i2c_address):
@@ -184,19 +202,6 @@ class CB_VL53L1X(BaseSensor):
             return True
         else:
             return False
-
-    @property
-    def i2c_address(self):
-        return self._i2c_address
-
-    @i2c_address.setter
-    # Sets the address of the board. This presumes that we start from a place of all boards being shut off.
-    def i2c_address(self, i2c_address):
-        # If it's in "0xYY" format, convert it to a base 16 int.
-        if isinstance(i2c_address, str):
-            self._i2c_address = int(i2c_address, base=16)
-        else:
-            self._i2c_address = i2c_address
 
     @property
     def enable_board(self):
@@ -242,6 +247,7 @@ class CB_VL53L1X(BaseSensor):
         # Stop from ranging
         self.stop_ranging()
 
+
 class TFMini(BaseSensor):
     def __init__(self, board_options):
         super().__init__(board_options)
@@ -251,8 +257,12 @@ class TFMini(BaseSensor):
         }
 
         # Create the sensor object.
-        self._sensor_obj = TFminiI2C(self.i2c_bus, self._i2c_address)
+        self._logger.debug("Creating TFMini object on bus {}, address {}".format(self.i2c_bus, hex(self.i2c_address)))
+        self._sensor_obj = TFminiI2C(self.i2c_bus, self.i2c_address)
         self._sensor_obj.setUnit(0x01)  # Make sure we're set to centimeters.
+        self._sensor_obj.reset()
+        sleep(0.5)
+        self._logger.debug("Test reading: {}".format(self.range))
 
     # This sensor doesn't need an enable, do nothing.
     def enable(self):
@@ -267,8 +277,36 @@ class TFMini(BaseSensor):
         if monotonic() - self._previous_timestamp < 0.2:
             return self._previous_reading
         else:
-            reading = self._sensor_obj.readDistance()
-            if reading is None:
-                return None
+            reading = self._clustered_read()
+            # reading = self._sensor_obj.readAll()
+            self._logger.debug("TFmini read values: {}".format(reading))
+            if reading[1] == 65535 and reading[2] < 100:
+                # Insufficient reflection strength means the sensor isn't hitting anything, therefore,
+                # door is open and bay is clear.
+                return "Door Open"
+            elif reading[1] == 65534 and ( reading[2] == 65535 or reading[2] == -1):
+                # No reading because the sensor is washed out.
+                return "Signal Saturation"
+            elif reading[1] == 65532:
+                # Ambient lights are too bright
+                return "Ambient Saturation"
             else:
-                return Quantity(reading, self._ureg.centimeter)
+                self._previous_reading = Quantity(reading[1], self._ureg.centimeter)
+                self._previous_timestamp = monotonic()
+                return self._previous_reading
+
+    # The TFMini can sometimes return wonky, bullshit answers. Chuck those.
+    def _clustered_read(self):
+        stable_count = 0
+        i = 0
+        previous_read = self._sensor_obj.readAll()
+        start = monotonic()
+        while stable_count < 3:
+            reading = self._sensor_obj.readAll()
+            if reading[1] == previous_read[1]:
+                stable_count += 1
+            previous_read = reading
+            i += 1
+        self._logger.debug("Took {} cycles in {}s to get stable reading.".format(i, monotonic() - start))
+        return previous_read
+
