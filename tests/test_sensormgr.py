06fe57a7988c8fc53ba6e23c9ef5e1929881b54c
Chris <chris@chrisgill.net>
2025-02-21 07:21:57 -0500

    0.4.2a3 * Minor tweak to sensormgr test.

--- tests/test_sensormgr.py
+++ tests/test_sensormgr.py
@@ -26,8 +26,29 @@ good_sensor_config = {
             "enable_pin": 1,
             "distance_mode": "long",
             "timing": "200 ms"
-            }
-        }
+            },
+    "front": {
+        "name": "Front",
+        "hw_type": "VL53L1X",
+        "i2c_bus": 1,
+        "i2c_address": 0x31,
+        "enable_board": 0x58,
+        "enable_pin": 2,
+        "distance_mode": "long",
+        "timing": "200 ms"
+    },
+    "middle": {
+        "name": "Middle",
+        "hw_type": "VL53L1X",
+        "i2c_bus": 1,
+        "i2c_address": 0x32,
+        "enable_board": 0x58,
+        "enable_pin": 3,
+        "distance_mode": "long",
+        "timing": "200 ms"
+    }
+}
+
 good_i2c_config = {
             "bus": 1,
             "enable": "D20",
@@ -36,20 +57,8 @@ good_i2c_config = {
             "wait_reset": 10
         }
 
-# From https://github.com/pytest-dev/pytest/issues/5502#issuecomment-1803676152
-# This fixes "I/O operation on closed file" issues during exit.
-@pytest.fixture(scope='session', autouse=True)
-def cleanup_logging_handlers():
-    try:
-        yield
-    finally:
-        for handler in logging.root.handlers[:]:
-            if isinstance(handler, logging.StreamHandler):
-                logging.root.removeHandler(handler)
-
-
 @pytest.fixture
-def new_sensormgr(cleanup_logging_handlers):
+def new_sensormgr():
     """ Fixture used to get a new sensor manager """
 
     created_objects = []
@@ -72,34 +81,34 @@ def new_sensormgr(cleanup_logging_handlers):
     for object in created_objects:
         del(object)
 
-# def test_sensormgr_init(new_sensormgr):
-#     """
-#     Can the the Sensor Manager be initialized.
-#     """
-#     sensormgr = new_sensormgr(
-#         sensor_config=good_sensor_config,
-#         i2c_config=good_i2c_config
-#     )
-#
-#     assert isinstance(sensormgr, cobrabay.CBSensorMgr)
-#
-# def test_sensormgr_range(new_sensormgr):
-#     """
-#     Load the sensor manager, loop it once and ensure it returns a SensorResponse.
-#     """
-#     sensormgr = new_sensormgr(
-#         sensor_config=good_sensor_config,
-#         i2c_config=good_i2c_config
-#     )
-#
-#     # Put the RANGING command in the command queue.
-#     q_cbsmcontrol.put((cobrabay.const.SENSTATE_RANGING,None))
-#     sensormgr.loop()
-#
-#     # Check the return queue.
-#     scan_data = q_cbsmdata.get()
-#
-#     assert isinstance(scan_data, cobrabay.datatypes.SensorResponse)
+def test_sensormgr_init(new_sensormgr):
+    """
+    Can the Sensor Manager be initialized.
+    """
+    sensormgr = new_sensormgr(
+        sensor_config=good_sensor_config,
+        i2c_config=good_i2c_config
+    )
+
+    assert isinstance(sensormgr, cobrabay.CBSensorMgr)
+
+def test_sensormgr_range(new_sensormgr):
+    """
+    Load the sensor manager, loop it once and ensure it returns a SensorResponse.
+    """
+    sensormgr = new_sensormgr(
+        sensor_config=good_sensor_config,
+        i2c_config=good_i2c_config
+    )
+
+    # Put the RANGING command in the command queue.
+    q_cbsmcontrol.put((cobrabay.const.SENSTATE_RANGING,None))
+    sensormgr.loop()
+
+    # Check the return queue.
+    scan_data = q_cbsmdata.get()
+
+    assert isinstance(scan_data, cobrabay.datatypes.SensorResponse)
 
 def test_sensormgr_bus_disable(new_sensormgr):
     sensormgr = new_sensormgr(
